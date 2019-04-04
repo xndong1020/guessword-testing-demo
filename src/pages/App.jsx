f99b9c962764cfe3b3a83ff6c8002c54a40fc960
isdance <isdance2004@hotmail.com>
2019-04-04 20:36:19 +1100

    done unit testing for components

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,57 @@
-import React, { Component } from 'react';
-import './App.scss';
+import React, { Component } from 'react'
+import './App.scss'
 
 class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  handleIncrement = () => {
+    this.setState(prevState => {
+      let { counter, error } = prevState
+      // clear error if error is not empty
+      const newState = error
+        ? {
+            counter: counter + 1,
+            error: ''
+          }
+        : { counter: counter + 1 }
+      return newState
+    })
+  }
+
+  handleDecrement = () => {
+    this.setState(prevState => {
+      let { counter, error } = prevState
+      if (counter - 1 >= 0) {
+        return {
+          counter: counter - 1
+        }
+      }
+      // if you reach here, the counter value must be 0
+      if (!error) {
+        return {
+          error: `the counter can't go below zero`
+        }
+      }
+    })
+  }
+
   render() {
+    const { counter, error } = this.state
     return (
       <div data-test="component-app">
+        <div data-test="counter-display">Counter current is: {counter}</div>
+        <button data-test="increment-button" onClick={this.handleIncrement}>
+          increment
+        </button>
+        <button data-test="decrement-button" onClick={this.handleDecrement}>
+          decrement
+        </button>
+        {error && <div data-test="error-display">{error}</div>}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
